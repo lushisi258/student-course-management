87fc1db8b92ac7aa1ddea678cf1181b57db31073
lidingxin <lushisi258@qq.com>
2024-05-28 13:23:27 +0800

    修改

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,13 @@ import os
 from flask import Flask
 from .views import views
 from .models import db
+from config import password
 import pymysql
 pymysql.install_as_MySQLdb()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:25858.asd@localhost/scmdb'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:'+password+'@localhost/scmdb'
     db.init_app(app)
 
     app.register_blueprint(views)
--- config.py
+++
@@ -1,4 +0,0 @@
-import os
-
-SECRET_KEY = os.urandom(24)
-SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
